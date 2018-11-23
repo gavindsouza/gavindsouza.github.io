a217c343f7e40a990c6ff14d8ae361cdb0de8bf5
Gavin D'souza <gavin18d@gmail.com>
2018-11-23 16:33:32 +0530

    [CHANGES] added PWA arch manifest, added babel in local modules\n[REMOVED] img ==> images/icons/

--- js/user.js
+++ js/user.js
@@ -1,12 +1,16 @@
-let request = new XMLHttpRequest();
-request.open('get', "https://api.github.com/users/gavindsouza/repos");
-request.onload = OnSuccessfulLoadofJSON;
-request.onerror = OnUnsuccessfulLoadofJSON;
-request.send();
+const githubURL = "https://api.github.com/users/gavindsouza/repos";
 
-function OnSuccessfulLoadofJSON() {
-    let myInfo = JSON.parse(request.responseText);
+
+window.addEventListener('load', event => {
+    loadGitHubData();
+})
+
+async function loadGitHubData() {
+    const result = await fetch(githubURL);
+    const myInfo = await result.json();
+
     console.log("Number of public Repositories: ", myInfo.length);
+    
     forks = []; repos = [];
     for (let i = 0; i < myInfo.length; i++) {
         if (myInfo[i]["fork"] == true)
@@ -19,6 +23,30 @@ function OnSuccessfulLoadofJSON() {
 
 }
 
-function OnUnsuccessfulLoadofJSON() {
-    console.log("why is this happening to us :(\nPleasee tweet me @itbegavin, thanks :)");
-}
+
+
+
+// let request = new XMLHttpRequest();
+// request.open('get', githubURL);
+// request.onload = OnSuccessfulLoadofJSON;
+// request.onerror = OnUnsuccessfulLoadofJSON;
+// request.send();
+
+// function OnSuccessfulLoadofJSON() {
+//     let myInfo = JSON.parse(request.responseText);
+//     console.log("Number of public Repositories: ", myInfo.length);
+//     forks = []; repos = [];
+//     for (let i = 0; i < myInfo.length; i++) {
+//         if (myInfo[i]["fork"] == true)
+//             forks.push(myInfo[i]["name"]);
+//         else
+//             repos.push(myInfo[i]["name"]);
+//     }
+
+//     console.log(forks, repos);
+
+// }
+
+// function OnUnsuccessfulLoadofJSON() {
+//     console.log("why is this happening to us :(\nPleasee tweet me @itbegavin, thanks :)");
+// }
