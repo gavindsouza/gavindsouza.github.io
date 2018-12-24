0de00a99bbc1be71ad8faecb5e95fe41a4d81974
Gavin Dsouza <gavin18d@gmail.com>
2018-12-24 14:40:47 +0530

    [CHANGES] cache time updation sw; kaggle update links.js [ADDED] old version of github.js to _refs

--- serviceworker.js
+++ serviceworker.js
@@ -5,7 +5,7 @@ ref: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-workbox.setConfig({ debug: true });   
+workbox.setConfig({ debug: false });   
 
 workbox.routing.registerRoute(
     'https://api.github.com/users/gavindsouza/repos',
@@ -17,7 +17,7 @@ workbox.routing.registerRoute(
     /.*\.(?:js|css|ico|png)$/,
     workbox.strategies.cacheFirst({
         cacheName: 'mega-cache',
-        cacheExpiration: {maxEntries: 20, maxAgeSeconds: 12 * 60 * 60},
+        cacheExpiration: {maxEntries: 20, maxAgeSeconds: 0},
         cacheableResponse: {statuses: [0, 200]}
     })
 );
