33eeadc3477d281e1a01c3dd892db0cc7cf63b62
Gavin D'souza <gavin18d@gmail.com>
2021-06-27 14:35:41 +0530

    chore: Remove dist and temp folders from version control

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - gavin's Blog",
-  "siteUrl": "https://gavindsouza.in",
-  "version": "0.7.23",
-  "catchLinks": true
-}
